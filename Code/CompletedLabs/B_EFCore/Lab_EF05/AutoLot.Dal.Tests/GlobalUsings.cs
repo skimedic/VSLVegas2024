4e9693decf4f14ceb233be6f0ed5578be390d79d
Philip Japikse <skimedic@gmail.com>
2024-02-07 07:08:37 -0500

    initial checkin

---
+++ Code/CompletedLabs/B_EFCore/Lab_EF05/AutoLot.Dal.Tests/GlobalUsings.cs
@@ -0,0 +1,8 @@
+// Copyright Information
+// ==================================
+// AutoLot8 - AutoLot.Dal.Tests - GlobalUsings.cs
+// All samples copyright Philip Japikse
+// http://www.skimedic.com 2024/2/4
+// ==================================
+
+global using Xunit;
---
+++ Code/CompletedLabs/C_RazorPages/Lab_RP01/AutoLot.Services/Class1.cs
@@ -0,0 +1,13 @@
+﻿// Copyright Information
+// ==================================
+// AutoLot8 - AutoLot.Services - Class1.cs
+// All samples copyright Philip Japikse
+// http://www.skimedic.com 2023/11/25
+// ==================================
+
+namespace AutoLot.Services;
+
+public class Class1
+{
+
+}
